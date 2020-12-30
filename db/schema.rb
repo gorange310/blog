8556e3c82029d5736bf15122f50391ee32ca2af3
Gene <gorange310@gmail.com>
2020-12-30 16:58:32 +0800

    Add bcrypt and has_secure_password to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_075526) do
+ActiveRecord::Schema.define(version: 2020_12_30_083940) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_075526) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
