25fa9927f0e55c0b5b6a834a60c579265ce5d043
Gene <gorange310@gmail.com>
2021-01-08 18:20:49 +0800

    add Model Comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
-  resources :posts
-  get 'signup', to: 'users#new'
+  resources :posts do
+    member do
+      post :create  # /cards/2/like
+    end
+
+    resources :comments, shallow: true,
+               except: [:index, :show, :new]
+  end
+
   resources :users, except: [:new]
+  get 'signup', to: 'users#new'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
