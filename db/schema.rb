2c4e8492e56d044bbab5cfb8ccc1e28822267ba5
Gene <gorange310@gmail.com>
2021-01-08 13:00:25 +0800

    add admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_084551) do
+ActiveRecord::Schema.define(version: 2021_01_08_044139) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -26,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_084551) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
