a7c16732e8e7c5780524eb019071416fbb519471
Gene <gorange310@gmail.com>
2021-01-19 20:11:45 +0800

    category valid test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_044139) do
+ActiveRecord::Schema.define(version: 2021_01_19_120932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "commenter_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -32,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_01_08_044139) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "comments", "posts"
 end
