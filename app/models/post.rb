1c7f41de2b1c675db5fb73c4a073693af1f05adb
Gene <gorange310@gmail.com>
2021-01-21 17:03:15 +0800

    post-to-categories-implementation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :post_categories
+  has_many :categories, through: :post_categories
   validates :title, presence: true
   validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_120932) do
+ActiveRecord::Schema.define(version: 2021_01_21_081948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,12 +30,22 @@ ActiveRecord::Schema.define(version: 2021_01_19_120932) do
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
+  create_table "post_categories", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "posts_categories", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
