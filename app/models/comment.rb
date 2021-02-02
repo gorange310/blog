f9a821e18b430c2a8733fce4eebbbfe80c3be1e8
Gene <gorange310@gmail.com>
2021-02-03 01:16:51 +0800

    rename column

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  belongs_to :guest, class_name: "User", foreign_key: "guest_id"
+  belongs_to :guest, class_name: "User"
 
   validates :content, presence: true
 
