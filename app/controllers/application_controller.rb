51aaaf0e05c655b5f7901bb30eaf8a24bb81594e
Gene <gorange310@gmail.com>
2021-02-18 23:04:23 +0800

    add link to different langueges at footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   helper_method :current_user, :logged_in?
-  
+  before_action :set_locale
+
+  def set_locale
+    # 可以將 ["en", "zh-TW"] 設定為 VALID_LANG 放到 config/environment.rb 中
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
   def current_user
     @current ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -16,7 +26,7 @@ class ApplicationController < ActionController::Base
       redirect_to login_path
     end
   end
-  
+
   private
   def not_found
     render file: "public/404.html", layout: false, status: 404
