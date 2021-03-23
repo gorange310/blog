aebf665156e037b65c260227fa210a45b3460214
Gene <gorange310@gmail.com>
2021-03-23 13:19:21 +0800

    Install action_text

--- app/javascripts/packs/application.js
+++ app/javascripts/packs/application.js
@@ -8,7 +8,9 @@ import "bootstrap"
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-    //require("channels")
+require("trix")
+require("@rails/actiontext")
+//require("channels")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,9 @@ class Post < ApplicationRecord
   has_many :categories, through: :post_categories
   validates :title, presence: true
   validates :content, presence: true
+
+  has_rich_text :content
+
   def self.search(keyword)
     where("title like '%#{keyword}%' or content like '%#{keyword}%'")
   end
